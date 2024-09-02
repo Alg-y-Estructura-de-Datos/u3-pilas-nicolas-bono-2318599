dead68ed4ea7e11fe0356f65c55e2716f2090d8a
NicolasBono <2318599@ucc.edu.ar>
2024-09-02 11:11:16 -0300

    ejclase

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
-
+#include "Pila/Pila.h"
 int main() {
-
-    cout << "Ejercicio N° 1" << endl;
-
-    return 0;
+string palabra;
+Pila<char>palabra1;
+cout<<"Decime la palabra \n";
+    cin>>palabra;
+    for (int i = 0; i < palabra.length(); ++i) {
+        palabra1.push(palabra[i]);
+    }
+    while (!palabra1.esVacia()){
+        cout<<palabra1.pop();
+    }
 }
